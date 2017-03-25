9fc574aa97ab391831c28a5b17277c8309390b53
BiXichao <bixichao@berrygenomics.com>
2017-03-25 15:46:10 +0800

    Better annodb config

--- annodb/models.py
+++ annodb/models.py
@@ -17,26 +17,8 @@ from lib.Cytoband import Cytoband
 from apps import AnnodbConfig
 # Create your models here.
 
-'''
-from mysite.settings import DATABASES
-mongoe.connect(
-    DATABASES['annodb']['NAME'],
-    host=DATABASES['annodb']['HOST'],
-    alias=DATABASES['annodb']['alias']
-)
-'''
 mongoe.connect(
     AnnodbConfig.db,
     host=AnnodbConfig.host,
     alias=AnnodbConfig.alias
 )
-
-def dbconnection(dbname, host='mongodb://localhost:27017', alias='annodb'):
-    """
-    这个函数已经可以删除了，没有用处了
-    """
-    try:
-        mongoe.connect(dbname, host=host, alias=alias)
-    except:
-        raise Exception(u"数据库链接失败")
-
