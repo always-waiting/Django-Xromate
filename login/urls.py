798b4e6eafcd64899b834344090878bebe0bf7f5
bixichao <bixichao@berrygenomics.com>
2017-03-27 12:07:28 +0800

    Add login with gitlab

--- login/urls.py
+++ login/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     url(r'^$', views.login, name='web_login'),
     url(r'github_login', views.github_login, name='github_login'),
     url(r'github_auth', views.github_auth, name='github_auth'),
+    url(r'gitlab_login', views.gitlab_login, name='gitlab_login'),
+    url(r'gitlab_auth', views.gitlab_auth,name='gitlab_auth'),
     url(r'logout', views.logout, name='web_logout')
 ]
--- login/views.py
+++ login/views.py
@@ -12,10 +12,17 @@ import logging
 logger = logging.getLogger('django')
 
 # Create your views here.
+# GITHUB
 GITHUB_CLIENTID = settings.GITHUB_CLIENTID
 GITHUB_CLIENTSECRET = settings.GITHUB_CLIENTSECRET
 GITHUB_CALLBACK = settings.GITHUB_CALLBACK
 GITHUB_AUTHORIZE_URL = settings.GITHUB_AUTHORIZE_URL
+# GITLAB
+GITLAB_CLIENTID = settings.GITLAB_CLIENTID
+GITLAB_CLIENTSECRET = settings.GITLAB_CLIENTSECRET
+GITLAB_CALLBACK = settings.GITLAB_CALLBACK
+GITLAB_AUTHORIZE_URL = settings.GITLAB_AUTHORIZE_URL
+GITLAB_URL = settings.GITLAB_URL
 
 def login(request):
     return render(request, 'login/login.html')
@@ -30,6 +37,18 @@ def _get_refer_url(request):
         del request.session['back_url']
     return refer_url
 
+def gitlab_login(request):
+    data = {
+        'client_id': GITLAB_CLIENTID,
+        'redirect_uri': GITLAB_CALLBACK,
+        'response_type': 'code',
+        'state': _get_refer_url(request),
+    }
+    gitlab_auth_url = '%s?%s'% (
+        GITLAB_AUTHORIZE_URL, urllib.urlencode(data)
+    )
+    return HttpResponseRedirect(gitlab_auth_url)
+
 
 def github_login(request):
     data = {
@@ -43,6 +62,35 @@ def github_login(request):
     )
     #print ('git_hub_auth_url',github_auth_url)
     return HttpResponseRedirect(github_auth_url)
+
+def gitlab_auth(request):
+    template_html = 'login/login.html'
+    if 'code' not in request.GET:
+        return render(request, template_html)
+    code = request.GET.get('code')
+    back_path = request.GET.get('state')
+    url = '%s%s' % (GITLAB_URL, 'oauth/token')
+    data = {
+        'client_id': GITLAB_CLIENTID,
+        'client_secret': GITLAB_CLIENTSECRET,
+        'code': code,
+        'grant_type': 'authorization_code',
+        'redirect_uri': GITLAB_CALLBACK,
+    }
+    binary_data = urllib.urlencode(data).encode('utf-8')
+    headers = {'Accep': 'application/json'}
+    req = urllib2.Request(url, binary_data, headers)
+    response = urllib2.urlopen(req)
+    result = json.loads(response.read())
+    access_token = result['access_token']
+    url = "%s%s?access_token=%s" % (GITLAB_URL,'api/v3/user', access_token)
+    response = urllib2.urlopen(url)
+    data = json.loads(response.read().decode('ascii'))
+    request.session['username'] = data['username']
+    request.session['is_authenticated'] = True
+    # 在这里处理数据库存储问题
+    return HttpResponseRedirect(back_path)
+
 
 def github_auth(request):
     template_html = 'login/login.html'
